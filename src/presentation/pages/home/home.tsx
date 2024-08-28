23ef6ee30546adcba228c914e2e539afd9d790f9
nicolastorre <10921378+nicolastorre@users.noreply.github.com>
2024-08-28 09:23:44 +0200

    add tailwind css

--- src/presentation/pages/home/home.tsx
+++ src/presentation/pages/home/home.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const HomePage: React.FC = () => {
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
+      <h1 className="text-3xl font-bold underline">Home page</h1>
     </div>
   );
 };
